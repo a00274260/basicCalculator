a3456f397d1d1e47354cbdbbd9571f101515aca1
a00274260 <a00274260@W202-A17.studentnet.ait.ie>
2020-01-27 16:17:57 +0000

    dceafwe

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -22,11 +22,20 @@ public class BasicCalculator {
 		return result;
 	}
 	
-	public static int subtract3Ints(int number1, int number2, int number3)
+	public static int multiply3Ints(int number1, int number2, int number3)
 	{
 		int result = 0;
 				
-		result = number1 - number2 - number3;
+		result = number1 * number2 * number3;
+		
+		return result;
+	}
+	
+	public static int divide3Ints(int number1, int number2, int number3)
+	{
+		int result = 0;
+				
+		result = number1 / number2 / number3;
 		
 		return result;
 	}
@@ -39,8 +48,14 @@ public class BasicCalculator {
 		int num2 = 2;
 		int num3 = 3;
 		int sumOf3Ints = 0;
+		int subtractionOf3Ints = 0;
+		int multiplicationOf3Ints = 0;
+		int divisionOf3Ints = 0;
 		
 		sumOf3Ints = add3Ints(num1, num2, num3);
+		subtractionOf3Ints = subtract3Ints(num1, num2, num3);
+		multiplicationOf3Ints = multiply3Ints(num1, num2, num3);
+		divisionOf3Ints = divide3Ints(num1, num2, num3)
 		
 		
 	}
