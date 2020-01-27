3a435a3421b99d8731a19bc29518f22fe3e20a49
a00274260 <a00274260@W202-A17.studentnet.ait.ie>
2020-01-27 16:17:51 +0000

    ejcaevu

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,5 +1,48 @@
 package baicCal;
 
 public class BasicCalculator {
+	
+	
+	public static int add3Ints(int number1, int number2, int number3) {
+		
+		int result = 0;	
+		
+		result = number1 + number2 + number3;
+		
+		return result;
+		
+	}
+	
+	public static int subtract3Ints(int number1, int number2, int number3)
+	{
+		int result = 0;
+				
+		result = number1 - number2 - number3;
+		
+		return result;
+	}
+	
+	public static int subtract3Ints(int number1, int number2, int number3)
+	{
+		int result = 0;
+				
+		result = number1 - number2 - number3;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int sumOf3Ints = 0;
+		
+		sumOf3Ints = add3Ints(num1, num2, num3);
+		
+		
+	}
 
 }
